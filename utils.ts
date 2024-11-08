4f033675041e4e1502fd444bdca17a52febe6a2d
ugwujoe <dnlfor2010@yahoo.com>
2024-11-08 09:29:34 +0100

    input validation

--- utils.ts
+++ utils.ts
@@ -1,8 +1,10 @@
 export const validatePassword = (password: string) => {
-    const regex = /^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
-    return regex.test(password);
-  }
-  
+  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%?&])[A-Za-z\d@$!%?&]{8,}$/;
+  return regex.test(password);
+
+
+}
+
  export  const validateEmail = (email: string) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
